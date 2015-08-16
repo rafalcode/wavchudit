a5406b2284a48af19e6020a94361693884595672
Ramon Fallon <ramonfallon@gmail.com>
2015-08-16 11:25:41 +0200

    dual direction started, not working yet

--- wtxslice.c
+++ wtxslice.c
@@ -32,6 +32,17 @@ typedef struct
     int byid; // BYtes_In_Data;
 } wh_t; /* wav header type */
 
+void usage(void)
+{
+        printf("Usage: Transfers the end chunk of one wav and inserts it at beginning of second.\n");
+        printf("3 obligatory args and one optional switch:\n");
+        printf("\t1) Name of first (earlier) wavfile 2) Name of first (later) wavfile 3) mm:ss.hh string.\n");
+        printf("\tThe default mode is to transfer a chunk from first wav file to second, with time referenced\n");
+        printf("\tfrom end of first wav file.\n");
+        printf("\t Optional switch: \"-b\" meaning \"backward\" transfers chunk from second file back to the end of\n");
+        printf("\tthe first. In this case, time point is referenced from beginning of second wavfile.\n"); 
+}
+
 tpt *s2tp(char *str) /* maps mins:seconds.centiseconds string into tpt struct: NOTE: a mapping not a conversion */
 {
     tpt *p=malloc(sizeof(tpt));
@@ -162,20 +173,129 @@ int dhdrchk(wh_t *hdr1, wh_t *hdr2) /* double header check, as well as sanity, t
     return 0;
 }
 
-int main(int argc, char *argv[])
+void tx(char *wf1, char *wf2, char *timestr, unsigned char backw)
 {
-    if(argc != 4) {
-        printf("Usage: Transfers the end chunk of one wav and inserts it at beginning of second.\n");
-        printf("3 args: 1) Name of first (earlier) wavfile 2) Name of first (later) wavfile 3) mm:ss.hh string.\n");
-        exit(EXIT_FAILURE);
-    }
-
     /* OK, we have two wav files, it's important that they have the same sampling and such parameters.
      * It's going to be a rare case if they don't, but , BUT ... good practice declares that we should.
      * This means we'll open both at same time. First however, we're goig to stat them using stat.h */
     struct stat fsta;
     size_t tstatbyid[2]={0};
     int i;
+    if(stat(wf1, &fsta) == -1) {
+        fprintf(stderr,"Can't open input file %s", wf1);
+        exit(EXIT_FAILURE);
+    }
+    tstatbyid[0]=fsta.st_size-44;
+    if(stat(wf2, &fsta) == -1) {
+        fprintf(stderr,"Can't open input file %s", wf2);
+        exit(EXIT_FAILURE);
+    }
+    tstatbyid[1]=fsta.st_size-44;
+
+    /* OK. as yet we don't know if they are decent WAV files, and wther they have the same parameters
+       Note that we open them as a matter of course, because the stat command has already checked for basic file trouble. */
+    FILE *wav1fp = fopen(wf1,"rb"), *wav2fp = fopen(wf2,"rb");
+
+    wh_t *hdr1=malloc(sizeof(wh_t)), *hdr2=malloc(sizeof(wh_t));
+    if ( fread(hdr1, sizeof(wh_t), sizeof(unsigned char), wav1fp) < 1 ) {
+        printf("Can't read %s's file header. Bailing out.\n", wf1);
+        exit(EXIT_FAILURE);
+    }
+    if ( fread(hdr2, sizeof(wh_t), sizeof(unsigned char), wav2fp) < 1 ) {
+        printf("Can't read %s's file header. Bailing out.\n", wf2);
+        exit(EXIT_FAILURE);
+    }
+    if( dhdrchk(hdr1, hdr2)) { /* check both wavheaders, they need to match */
+        printf("Error. Wav header parameter mismatch. Aborting.\n"); 
+        exit(EXIT_FAILURE);
+    }
+    /* with that check done we can just choose any of the two to calculate a robust byps */
+    int calcbyps= hdr1->sampfq*hdr1->nchans*(hdr1->bipsamp/8);
+
+    tpt *p=s2tp(timestr);
+#ifdef DBG
+    printf("Specified timept: mins=%d, secs=%d, centisecs=%d\n", p->m, p->s, p->c);
+#endif
+    int point= calcbyps*(p->m*60 + p->s) + p->c*calcbyps/100;
+
+    if(point >= tstatbyid[0]) {
+        printf("Error. Specified timept comes after length of first (earlier) wav. Aborting.\n");
+        exit(EXIT_FAILURE);
+    }
+    size_t slicesz= point; /* for clarity */
+    size_t newwavsz= tstatbyid[1]+endslicesz;
+    if(backw) {
+        fr2=1;
+        twavfp=wav2fp;
+    }
+    unsigned char *bf=malloc(tstatbyid[oner2]);
+    if ( fread(bf, tstatbyid[oner2], sizeof(unsigned char), wav1fp) < 1 ) {
+        printf("Sorry, trouble putting first input file into array. Aborting\n"); 
+        exit(EXIT_FAILURE);
+    }
+
+    /* instead of closing the file, we'll rewind and write over, wiht truncated data. Erase that. We'll close and reopen */
+    fclose(wav1fp);
+    wav1fp = fopen(wf1],"wb");
+    rewind(wav1fp);
+    hdr1->byid = point;
+    hdr1->glen=hdr1->byid+36;
+    fwrite(hdr1, sizeof(unsigned char), 44, wav1fp);
+    fwrite(bf, sizeof(unsigned char), point, wav1fp);
+    fclose(wav1fp);
+    free(hdr1);
+
+    /* another buffer, but let's avoid two big ones open */
+    unsigned char *bf2=malloc(endslicesz);
+    memcpy(bf2, bf+point, endslicesz); 
+    free(bf);
+    bf2=realloc(bf2, newwav2sz);
+    if ( fread(bf2+endslicesz, tstatbyid[1], sizeof(unsigned char), wav2fp) < 1 ) {
+        printf("Sorry, trouble putting second wav file into array. Aborting\n"); 
+        exit(EXIT_FAILURE);
+    }
+    hdr2->byid += endslicesz;
+    hdr2->glen=hdr2->byid+36;
+    fclose(wav2fp);
+    wav2fp = fopen(wf2,"wb");
+    fwrite(hdr2, sizeof(unsigned char), 44, wav2fp);
+    fwrite(bf2, sizeof(unsigned char), newwav2sz, wav2fp);
+    fclose(wav2fp);
+    free(bf2);
+    free(hdr2);
+    free(p);
+
+    return 0;
+}
+int main(int argc, char *argv[])
+{
+    if((argc != 4) & (argc != 5) ) {
+        usage();
+        exit(EXIT_FAILURE);
+    }
+    unsigned char backw=0;
+    if(argc == 5) {
+        if(strcmp("-b", argv[4])) {
+            usage();
+            exit(EXIT_FAILURE);
+        } else
+            backw=1;
+    }
+
+    /* here is how we show the difference in transfer directions */
+    if(backw)
+        tx(argv[1], argv[1], argv[3], backw);
+    else
+        tx(argv[1], argv[2], argv[3], backw);
+
+
+
+    /* OK, we have two wav files, it's important that they have the same sampling and such parameters.
+     * It's going to be a rare case if they don't, but , BUT ... good practice declares that we should.
+     * This means we'll open both at same time. First however, we're goig to stat them using stat.h */
+    struct stat fsta;
+    size_t tstatbyid[2]={0};
+    int i, fr2=0 /*from two, i.e from second wav */;
     for(i=0;i<2;++i) {
         if(stat(argv[i+1], &fsta) == -1) {
             fprintf(stderr,"Can't open input file %s", argv[i+1]);
@@ -185,7 +305,7 @@ int main(int argc, char *argv[])
     }
 
     /* OK. as yet we don't know if they are decent WAV files, and wther they have the same parameters
-    Note that we open them as a matter of course, because the stat command has already checked for basic file trouble. */
+       Note that we open them as a matter of course, because the stat command has already checked for basic file trouble. */
     FILE *wav1fp = fopen(argv[1],"rb"), *wav2fp = fopen(argv[2],"rb");
 
     wh_t *hdr1=malloc(sizeof(wh_t)), *hdr2=malloc(sizeof(wh_t));
@@ -193,6 +313,7 @@ int main(int argc, char *argv[])
         printf("Can't read %s's file header. Bailing out.\n", argv[1]);
         exit(EXIT_FAILURE);
     }
+    FILE *twavfp=wav1fp;
     if ( fread(hdr2, sizeof(wh_t), sizeof(unsigned char), wav2fp) < 1 ) {
         printf("Can't read %s's file header. Bailing out.\n", argv[2]);
         exit(EXIT_FAILURE);
@@ -207,15 +328,19 @@ int main(int argc, char *argv[])
     printf("Specified timept: mins=%d, secs=%d, centisecs=%d\n", p->m, p->s, p->c);
 #endif
     int calcbyps= hdr1->sampfq*hdr1->nchans*(hdr1->bipsamp/8);
-    int point=calcbyps*(p->m*60 + p->s) + p->c*calcbyps/100;
+    int point= calcbyps*(p->m*60 + p->s) + p->c*calcbyps/100;
     if(point >= tstatbyid[0]) {
         printf("Error. Specified timept comes after length of first (earlier) wav. Aborting.\n");
         exit(EXIT_FAILURE);
     }
-    size_t endslicesz=tstatbyid[0]-point;
-    size_t newwav2sz=tstatbyid[1]+endslicesz;
-    unsigned char *bf=malloc(tstatbyid[0]);
-    if ( fread(bf, tstatbyid[0], sizeof(unsigned char), wav1fp) < 1 ) {
+    size_t slicesz= point; /* for clarity */
+    size_t newwavsz= tstatbyid[1]+endslicesz;
+    if(backw) {
+        fr2=1;
+        twavfp=wav2fp;
+    }
+    unsigned char *bf=malloc(tstatbyid[oner2]);
+    if ( fread(bf, tstatbyid[oner2], sizeof(unsigned char), wav1fp) < 1 ) {
         printf("Sorry, trouble putting first input file into array. Aborting\n"); 
         exit(EXIT_FAILURE);
     }
