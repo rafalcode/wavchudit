c7093e1a2be7df32e4e3712a04c021b6fc304774
rafalcode <ramonfallon@gmail.com>
2019-12-12 21:29:24 +0000

    chanceraw working, but in fact, it can't really be used for wavtable raw file, which are just too small a hefty mutliplier is needed for them

--- chanceraw.c
+++ chanceraw.c
@@ -6,8 +6,13 @@
 #include <string.h>
 #include <sys/time.h>
 #include <sys/stat.h>
+#include <dirent.h> 
 
-#define ONSZ 32 // otuput filename size.
+#define ONSZ 32 // output filename size.
+#define GBUF 128
+#define nr 2
+#define nc 2
+#define nb 3
 
 typedef struct /* wavheader type: wh_t */
 {
@@ -49,6 +54,26 @@ char *mkon(char *inpfn)  /* make output filename ... some convenient */
     return on;
 }
 
+char *mktmpd(void)
+{
+    struct timeval tsecs;
+    gettimeofday(&tsecs, NULL);
+    char *myt=calloc(14, sizeof(char));
+    strncpy(myt, "tmpdir_", 7);
+    sprintf(myt+7, "%lu", tsecs.tv_usec);
+
+    DIR *d;
+    while((d = opendir(myt)) != NULL) { /* NULL is returned if a directory with this name does not exist. That is waht we want, we do not want to clobber any existing directory */
+        gettimeofday(&tsecs, NULL);
+        sprintf(myt+7, "%lu", tsecs.tv_usec);
+        closedir(d);
+    }
+    // closedir(d); // this causing errors, because a returned NULL is a failure ... no directory will have been opened at this point, as it needs to have existed.
+    mkdir(myt, S_IRWXU);
+
+    return myt;
+}
+
 int main(int argc, char *argv[])
 {
     if(argc == 1) {
@@ -58,52 +83,69 @@ int main(int argc, char *argv[])
 
     /* The first check is to see if both these files are compatible. I.e. must have same sample frequencies and the like.
      * I was goign to check to see which was the smallest, but actually right now, we're going to pull them both into memory */
-    int i;
+    int i, j, k;
     FILE *inrawfp;
 
-    /* first wav file dealt with separately */
     /* Before opening, let's use stat on the wav file */
     struct stat fsta;
     if(stat(argv[1], &fsta) == -1) {
         fprintf(stderr,"Can't stat input file %s", argv[1]);
         exit(EXIT_FAILURE);
     }
-
-    inrawfp = fopen(argv[1],"rb");
+    size_t whsz=sizeof(wh_t);
+    size_t trsz=sizeof(char); // transfer size
+    printf("size of raw file is: %zu\n", fsta.st_size);
+    char *buf=malloc(fsta.st_size);
+    inrawfp = fopen(argv[1], "rb");
     if ( inrawfp == NULL ) {
         fprintf(stderr,"Can't open input file %s", argv[1]);
         exit(EXIT_FAILURE);
     }
-    if ( fread(inhdr1, sizeof(wh_t), sizeof(char), inrawfp) < 1 ) {
-        printf("Can't read file header\n");
+    if(fread(buf, fsta.st_size, trsz, inrawfp) < 1) {
+        printf("Sorry, trouble putting input file into buffer.\n"); 
         exit(EXIT_FAILURE);
     }
-    if(inhdr1->byid != statbyid) {
-        printf("header and stat conflicting on size of data payload. Pls revise.\n"); 
-        exit(EXIT_FAILURE);
-    }
-
-    /* decide an output file name */
-    char *outfn=mkon(argv[1]);
-    FILE *outwavfp= fopen(outfn,"wb");
-    char *bf=malloc(inhdr1->byid);
-    if ( fread(bf, inhdr1->byid, sizeof(char), inrawfp) < 1 ) { /* Yup! we slurp in the baby! */
-        printf("Sorry, trouble putting input file into array\n"); 
-        exit(EXIT_FAILURE);
-    }
-    /* close input file, it's in our buffer now */
     fclose(inrawfp);
-    /* but we write out to outfile almost immediately */
-    fwrite(bf, sizeof(char), inhdr1->byid, outwavfp);
-    fclose(outwavfp);
-    /* and now we don;t need the buffer */
-    wh_t *inhdr1=malloc(sizeof(wh_t));
-    size_t statglen=fsta.st_size-8; //filesz less 8
-    size_t statbyid=statglen-36; // filesz less 8+36, so that's less the wav header
-    free(bf);
 
-    free(inhdr1);
-    free(outfn);
+    wh_t *hd=calloc(1, whsz);
+    memcpy(hd->fstr, "WAVEfmt ", 8*sizeof(char));
+    memcpy(hd->id, "RIFF", 4*sizeof(char));
+    memcpy(hd->datastr, "data", 4*sizeof(char));
+    hd->fmtnum = 16;
+    hd->pcmnum = 1;
+    hd->bipsamp = 16;
+    hd->bypc = hd->bipsamp/8;
+    hd->byid=fsta.st_size;
+    hd->glen = hd->byid+36;
 
+    int r[nr]={44100, 48000};
+    int c[nc]={1, 2};
+    short b[nb]={16, 24, 32};
+
+    char *tmpd=mktmpd();
+    char *fn=calloc(GBUF, sizeof(char));
+    FILE *outwavfp;
+
+    for(i=0;i<nr;++i) {
+        for(j=0;j<nc;++j) {
+            for(k=0;k<nb;++k) {
+                hd->sampfq = r[i];
+                hd->nchans = c[j];
+                hd->bipsamp = b[k];
+                sprintf(fn, "%s/r%i_c%i_b%i.wav", tmpd, r[i], c[j], b[k]);
+                outwavfp = fopen(fn,"wb");
+                fwrite(hd, trsz, whsz, outwavfp);
+                fwrite(buf, trsz, fsta.st_size, outwavfp);
+                fclose(outwavfp);
+                printf("\"%s\" %d %d %d\n", hd->datastr, hd->nchans, hd->sampfq, hd->bipsamp);
+            }
+        }
+    }
+    printf("INFO: various trial wav's to be put into folder %s.\n", tmpd);
+
+    free(buf);
+    free(hd);
+    free(tmpd);
+    free(fn);
     return 0;
 }
